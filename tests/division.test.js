c0ede6a27b8b8c55df3523e6623f6cfd347bd9fd
Roberto Nuñez <rcorralesn@gmail.com>
2024-07-10 12:09:01 -0600

    add new test

--- tests/division.test.js
+++ tests/division.test.js
@@ -11,6 +11,12 @@ test('divides 8 / 2 to equal 4', () => {
 
     expect(divide(8, 2)).toBe(4);
 });
+test('divides 8 / 1 to equal 8', () => {
+    console.log(`[${timestamp()}] Running divide test`);
+
+    expect(divide(8, 1)).toBe(8);
+});
+
 
 test('divides by 0 to throw error', () => {
     console.log(`[${timestamp()}] Running divide test`);
